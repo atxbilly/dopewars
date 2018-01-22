8283e9f75cd2a41449f6272a30d47c11025e0dee
Brad Harris <bradharris512@gmail.com>
2018-01-22 13:28:44 -0600

    Retitled

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET home page */
 const index  = function(req, res){
-  res.render('index', {title: 'Dopewars'})
+  res.render('index', {title: 'International Dopewars'})
 };
 module.exports = {
   index
